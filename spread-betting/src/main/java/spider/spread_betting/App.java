7524cb007195a4b3ed438558cde64e2a6d8c1ad4
ramakanth77 <ram.77@live.com>
2018-02-15 00:26:40 +0530

    First Commit

---
+++ spread-betting/src/main/java/spider/spread_betting/App.java
@@ -0,0 +1,13 @@
+package spider.spread_betting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
