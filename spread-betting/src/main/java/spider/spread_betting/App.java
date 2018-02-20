14075e147f971ef31918e8e60c8b81a8507a2b87
ramakanth.reddy <ramakanth.reddy@N10596.intellectdesign.com>
2018-02-20 18:03:51 +0530

    Added code for new class

--- spread-betting/src/main/java/spider/spread_betting/App.java
+++ spread-betting/src/main/java/spider/spread_betting/App.java
@@ -1,13 +1,17 @@
 package spider.spread_betting;
 
+import org.apache.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	final static Logger logger = Logger.getLogger(App.class);
     public static void main( String[] args )
     {
         System.out.println( "Hello World! this is ramakanth" );
+        logger.info("hello");
     }
 }
--- spread-betting/src/main/java/spider/spread_betting/StartProject.java
+++ spread-betting/src/main/java/spider/spread_betting/StartProject.java
@@ -1,11 +1,20 @@
 package spider.spread_betting;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
+
 public class StartProject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
+		MongoClientURI uri = new MongoClientURI(
+				   "mongodb://ramakanth77:reddy1234@cluster0.mongodb.net/");
 		
+		MongoClient mongoClient = new MongoClient(uri);
+		MongoDatabase database = mongoClient.getDatabase("test");
+			
 		
 	}
 
