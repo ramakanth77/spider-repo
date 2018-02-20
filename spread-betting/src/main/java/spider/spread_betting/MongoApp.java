1185fbd18ad60be0a21086d2ac0b0ce0ea4420fe
ramakanth.reddy <ramakanth.reddy@N10596.intellectdesign.com>
2018-02-20 18:06:45 +0530

    added new files

---
+++ spread-betting/src/main/java/spider/spread_betting/MongoApp.java
@@ -0,0 +1,32 @@
+package spider.spread_betting;
+
+
+import org.apache.log4j.Logger;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+
+public class MongoApp {
+
+	final static Logger logger = Logger.getLogger(MongoApp.class);
+
+  public static void main(String[] args) throws Exception {
+
+	 MongoClientURI uri = new MongoClientURI("mongodb+srv://ramakanth77:reddy1234@spread-beatting-246kx.mongodb.net/test");
+
+	
+	MongoClient mongoClient = new MongoClient(uri);
+    @SuppressWarnings("deprecation")
+    MongoOperations mongoOps = new MongoTemplate(mongoClient, "test");
+
+    mongoOps.insert(new Person("Joe", 34));
+
+   logger.info(mongoOps.findOne(new Query(Criteria.where("name").is("Joe")), Person.class));
+
+    //mongoOps.dropCollection("person");
+  }
+}
